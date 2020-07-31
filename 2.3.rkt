8dbdaa91fd64e3f330a31d576642e1b6ca0b29a2
ImperialIain <istenson@ic.ac.uk>
2020-07-31 09:27:30 +0100

    More work on chapter 2.3

--- 2.3.rkt
+++ 2.3.rkt
@@ -38,6 +38,9 @@
 (define (sum? x)
   (and (pair? x) (eq? (car x) '+)))
 
+;; (define (exponentiation? exp)
+  ;; (and (pair? x) (eq? (car x) '+)))
+
 (define (variable? x) (symbol? x))
 
 (define (same-variable? v1 v2)
@@ -72,6 +75,10 @@
                         (deriv (multiplicand exp) var))
           (make-product (deriv (multiplier exp) var)
                         (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product (exponent exp)
+                       (make-exponentiation (base exp)
+                                            (make-sum (exponent exp) -1))))
         (else
          (error "unknown expression type -- DERIV" exp))))
 
@@ -95,5 +102,67 @@
 (base (make-exponentiation 'y 'x))
 (exponent (make-exponentiation 'y 'x))
 (exponentiation? (make-exponentiation 'y 'x))
-(= (make-exponentiation 'y 0) 1)
-;(deriv (make-exponentiation 'y 'x) 'x)
+;; (= (make-exponentiation 'y 0) 1)
+(deriv (make-exponentiation 'y 'x) 'x)
+(equals? '(* x (** y (+ x -1))) (deriv (make-exponentiation 'y 'x) 'x))
+(deriv '(* (* x y) (+ x 3)) 'x)
+
+(define (deriv2 exp var)
+  (display exp)(newline)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv2 (addend2 exp) var)
+                   (deriv2 (augend2 exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier2 exp)
+                        (deriv2 (multiplicand2 exp) var))
+          (make-product (deriv2 (multiplier2 exp) var)
+                        (multiplicand2 exp))))
+        ;; ((exponentiation? exp)
+         ;; (make-product (exponent exp)
+                       ;; (make-exponentiation (base exp)
+                                            ;; (make-sum (exponent exp) -1))))
+        (else
+         (error "unknown expression type -- DERIV2" exp))))
+
+(define (make-product2 m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (make-sum2 a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (multiplicand2 p)
+  (cond ((number? (cdr (cdr p))) p)
+        (else (cons '* (cdr (cdr p))))))
+
+(define (multiplier2 p) (cadr p))
+
+;; note that A & S seem to have the augend/addend
+;; definition backwards
+(define (augend2 p)
+   (cond ((number? (car (cdr (cdr p)))) (car (cdr (cdr p))))
+         (else (cons '+ (cdr (cdr p))))))
+
+;; define (augend2 s) (caddr s))
+
+(define (addend2 s) (cadr s))
+
+(equals? '(* 3 2) (multiplicand2 '(* 4 3 2)))
+(eq? 2 (augend2 '(+ 4 2)))
+(equals? '(+ 1 2 3 4) (augend2 '(+ 1 1 2 3 4)))
+
+;; (equals? '(+ 3 2) (augend2 '(+ 4 3 2)))
+;; (deriv2 '(* x y (+ x 3)) 'x)
+;; (deriv2 '(* x x x) 'x)
+
+;; sets
