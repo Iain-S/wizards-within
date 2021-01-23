0ff45578ccb7951d989300a46902732db557d222
IainS QMUL <i.stenson@qmul.ac.uk>
2021-01-23 16:49:31 +0000

    As far as exercise 3.19

--- 2.4.rkt
+++ 2.4.rkt
@@ -136,5 +136,121 @@
 
 (imag-part (make-from-real-imag 101 202))
 
+; Install the rectangular package.
+(define (install-rectangular-package)
+  ;; internal procedures
+  (define (real-part z) (car z))
+  (define (imag-part z) (cdr z))
+  (define (make-from-real-imag x y) 
+    (cons x y))
+  (define (magnitude z)
+    (sqrt (+ (square (real-part z))
+             (square (imag-part z)))))
+  (define (angle z)
+    (atan (imag-part z) (real-part z)))
+  (define (make-from-mag-ang r a)
+    (cons (* r (cos a)) (* r (sin a))))
+  ;; interface to the rest of the system
+  (define (tag x) 
+    (attach-tag 'rectangular x))
+  (put 'real-part '(rectangular) real-part)
+  (put 'imag-part '(rectangular) imag-part)
+  (put 'magnitude '(rectangular) magnitude)
+  (put 'angle '(rectangular) angle)
+  (put 'make-from-real-imag 'rectangular
+       (lambda (x y) 
+         (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'rectangular
+       (lambda (r a) 
+         (tag (make-from-mag-ang r a))))
+  'done)
 
-; 
+; Install the polar package.
+(define (install-polar-package)
+  ;; internal procedures
+  (define (magnitude z) (car z))
+  (define (angle z) (cdr z))
+  (define (make-from-mag-ang r a) (cons r a))
+  (define (real-part z)
+    (* (magnitude z) (cos (angle z))))
+  (define (imag-part z)
+    (* (magnitude z) (sin (angle z))))
+  (define (make-from-real-imag x y)
+    (cons (sqrt (+ (square x) (square y)))
+          (atan y x)))
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'polar x))
+  (put 'real-part '(polar) real-part)
+  (put 'imag-part '(polar) imag-part)
+  (put 'magnitude '(polar) magnitude)
+  (put 'angle '(polar) angle)
+  (put 'make-from-real-imag 'polar
+       (lambda (x y) 
+         (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'polar
+       (lambda (r a) 
+         (tag (make-from-mag-ang r a))))
+  'done)
+
+; Write put and get.
+;(define table '())
+;(define (put op type item)
+;  (append ))  ; Ah, not so easy.  How do we table.append(x)?
+
+; Mock put and get.
+(define (put op type item) 0)
+(define (get op type) 0)
+
+"Exercise 2.73: Symbolic differentiation."
+; From 2.3.3
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2)
+  (and (variable? v1)
+       (variable? v2)
+       (eq? v1 v2)))
+
+(define (deriv exp var)
+   (cond ((number? exp) 0)
+         ((variable? exp) 
+           (if (same-variable? exp var) 
+               1 
+               0))
+         (else ((get 'deriv (operator exp)) 
+                (operands exp) 
+                var))))
+
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+
+; Q1: Why can't we assimilate number? and variable? into data directed dispatch?
+; A1: Because there isn't any need and there is no operator to look up.
+
+; Q2: Write the procedures for derivatives of sums and products using the above.
+; A2:
+
+(define (install-derive-sum-prod)
+  ;; internal procedures
+  (define (deriv-prod exp var)
+    ; apply the product rule (assume only 2 operands)
+    (+ (* (cdr exp) (deriv (car exp) var))
+       ; NB I should have used make-sum, augend and addend for these.
+       (* (car exp) (deriv (cdr exp) var))))
+  (define (deriv-plus exp var)
+    '())
+  ;; register with the system
+  (put 'deriv '* deriv-prod)
+  (put 'deriv '+ deriv-plus)
+  )
+
+; Does this work the way I think it does?
+(define (return-plus) +)
+((return-plus) 1 0)
+
+; Does let work the way I think?
+;(let ((table '()))
+;  (define (put item table)
+;    (cons item table))
+;  (define (get)
+;    '()))
+
+;; ToDo Return to 2.4 and 2.5 when we have finished 3.3.3 and can write put and get.
--- 3.0.rkt
+++ 3.0.rkt
@@ -160,7 +160,9 @@
         (begin
           (set! tries 0)
           (dispatch message))
-        (begin (display tries) (if (= tries 2)
+        (begin
+          ;(display tries)
+          (if (= tries 2)
             call-the-cops
             (begin
               (set! tries (+ tries 1))
@@ -197,14 +199,20 @@
     (+ low (random range))))
 
 (define (estimate-integral P x1 x2 y1 y2 trials)
-  1)
+  (define (integral-test)
+    (P (random-in-range x1 x2)
+       (random-in-range y1 y2)))
+  (monte-carlo trials integral-test))
 
 (define (Pcircle x y)
   ; Check that the distance to the centre is < the radius
   (< (sqrt (+
-            (expt (abs (- x 1)) 2)
-            (expt (abs (- y 1)) 2)))
+            (expt (abs (- x 1.0)) 2.0)
+            (expt (abs (- y 1.0)) 2.0)))
      1))
+
+(define (Prect x y)
+  (< x 1))
 
 (Pcircle 1 1) ; should be #t
 (Pcircle 1.5 1.5) ; should be #t
@@ -216,9 +224,331 @@
   ; pi = area / r^2 and
   ; we are doing a unit circle so R^2 = 1 so
   ; pi = area
-
   ; note: a unit circle fits in a 2 x 2 box, not a 1 x 1 box
-  (estimate-integral Pcircle 0 2 0 2 trials) 
+  (* 4
+     ; Remember to use floats throughout so that (random) returns floats.
+     (estimate-integral Pcircle 0.0 2.0 0.0 2.0 trials)) 
   )
 
-(estimate-pi 1000)
+(estimate-pi 100000)  ; 3.14170...
+
+"Exercise 3.6 : A RNG that you can reset"
+(define (make-new-rand)
+  (define x 44)
+  (lambda (msg)
+    (if (eq? msg 'generate)
+        (begin (set! x (modulo (+ (* 44 x)
+                                  9) 223456))
+               x)
+        (lambda (reset-to)
+          (set! x reset-to)))))
+
+(define new-rand (make-new-rand))
+(new-rand 'generate) ; a)
+(new-rand 'generate)
+(new-rand 'generate)
+((new-rand 'reset) 44) ; Should be the same as a)
+(new-rand 'generate)
+
+"Exercise 3.7 : Joint accounts"
+(define (make-joint account orig-pwd new-pwd)
+  (if (number? ((account orig-pwd 'd) 0))
+      ; then orig-pwd is correct
+      (lambda (pwd action)
+        (if (eq? pwd new-pwd)
+            ; pwd is correct
+            (account orig-pwd action)
+            "Incorrect pwd"
+            ))
+      (lambda (b c)
+        "Incorrect orig-pwd")))
+
+(define peter-acc
+  (make-account-3 100 'mynameispeter))
+((peter-acc 'mynameispeter 'd) 20)
+
+(define paul-acc (make-joint peter-acc
+                             'mynameispeter
+                             'mynameispaul))
+
+(= 110 ((paul-acc 'mynameispaul 'w) 10))
+(eq? (paul-acc 'mynamezzispeter 'w) "Incorrect pwd")
+
+"Exercise 3.8 : Order of evaluation"
+; define a simple procedure, f, such that (+ (f 0) (f 1)) will return
+; 0 if the order of evaluation is left to right and 1 if right to left
+(define (make-f x)
+  (lambda (a)
+    (if (< a x)
+        (begin (set! x a)
+               x)
+        x)))
+
+(define f (make-f 1))
+
+(+ (f 0) (f 1))
+
+ (define f2 
+   (let ((called #f)) 
+     (lambda (x) 
+       (if called 
+           0 
+           (begin 
+             (set! called #t) 
+             x)))))
+
+(+ (f2 0) (f2 1))
+
+f2
+
+"3.9 Environments and Factorials"
+; From 1.2.1, we have to methods to compute factorials
+; Show the environment structures created by evaluating the fact of 6
+; for both methods.
+
+; recursive
+(define (factorial n)
+  (if (= n 1)
+      1
+      (* n (factorial (- n 1)))))
+; global env has factorial function that points to global env
+; E1 created by call to (factorial 6) with n=6. E1 points to global env.
+; E2 created by call to (factorial (- n 1)) with n=5. E2 points to global env.
+; ...
+; E6 created with n=1.  E6 points to global env.  Function returns 1.
+
+; and iterative
+(define (fact-iter product 
+                   counter 
+                   max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+                 (+ counter 1)
+                 max-count)))
+
+(define (fact2 n)
+  (fact-iter 1 1 n))
+
+; global env has fact2 function that points to global env
+; E1 created by call to (fact2 6) with n=5. E1 points to global env.
+; E2 created by call to (fact-iter 1 1 6). E2 points to global env.
+; E3 created by call to (fact-iter 1 2 6). E3 points to global env.
+; ...
+; E8 created by call to (fact-iter x 7 6). E8 points to global env.
+; function returns x
+
+"Exercise 3.10 Let and make-withdraw"
+; One way of making make-withdraw
+(define (make-withdraw-1 balance)
+  (lambda (amount)
+    (if (>= balance amount)
+        (begin (set! balance 
+                     (- balance amount))
+               balance)
+        "Insufficient funds")))
+
+; An alternative
+(define (make-withdraw-2 initial-amount)
+  (let ((balance initial-amount))
+    (lambda (amount)
+      (if (>= balance amount)
+          (begin (set! balance 
+                       (- balance amount))
+                 balance)
+          "Insufficient funds"))))
+
+; Recall that
+; (let ((⟨var⟩ ⟨exp⟩)) ⟨body⟩)
+; is interpreted as an alternate syntax for
+; (lambda (⟨var⟩) ⟨body⟩) ⟨exp⟩)
+
+; Illustrate how the two make-withdraw procedures above would handle
+(define W1 (make-withdraw 100))
+(W1 50)
+(define W2 (make-withdraw 100))
+
+; Show that the two versions create the same objects.
+; How do the environment structures differ?
+
+; 1
+; -------------
+; | global                                         <------------
+; | make-withdraw-1 ----> params: initial-amount, body..., env |
+; | W1
+; | W2
+; -------------
+
+
+
+"Exercise 3.12 Append!ing"
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+(define (append! x y)
+  (set-cdr! (last-pair x) y)
+  x)
+
+(define x (list 'a 'b))
+(define y (list 'c 'd))
+(define z (append x y))
+
+;z
+
+(cdr x)
+
+(define w (append! x y))
+
+;w
+
+(cdr x)
+
+"Exercise 3.13"
+(define (make-cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
+
+; Q) Draw a diagram to show the structure of 
+(define zz (make-cycle (list 'a 'b 'c))) ; done on paper
+
+; and explain what will happen if we were to run
+;(last-pair zz)?
+; A) It will never terminate
+
+"Exercise 3.14: mysteries"
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ((temp (cdr x)))
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
+; Q) What does mystery do?
+; A) It reverses a list
+(define vv (list 'a 'b 'c 'd))
+(define ww (mystery vv))
+vv ; Note that the first call to loop does (set-cdr! '(a b c d) '())
+ww
+
+"Exercise 3.16: counting pairs"
+(define (count-pairs x)
+  (if (not (pair? x))
+      0
+      (+ (count-pairs (car x))
+         (count-pairs (cdr x))
+         1)))
+; Q) Why is this implementation wrong? Show inputs of 3 pairs for which
+;    it will return 3, 4, 7 or never return.
+(count-pairs (list (list 1 2))) ; =3
+
+(define pair-z (list 3)) ; 1 pair
+(define pair-a (cons 2 pair-z)) ; 2 pairs -> (1 (2 null))
+(define pair-b (cons pair-a pair-z)) ; now 3 pairs (pair-a pair-a)
+(count-pairs pair-b) ; =4
+;(count-pairs pair-a)
+
+(define pair-3 pair-z)
+(define pair-2 (cons pair-z pair-z))
+(define pair-1 (cons pair-2 pair-2))
+(count-pairs pair-1) ;= 7
+
+(define pair-i (list 3)) ; 3,null
+(define pair-j (cons 2 pair-i)) ;2, pair-i
+(define pair-k (cons 1 pair-j)) ; 1, pair-j
+(count-pairs pair-k)
+(set-cdr! pair-i pair-k) ; the end of pair-i now wraps around to pair-k
+;(count-pairs pair-k) ; will never return
+
+"Exercise 3.17 : A correct version of count-pairs"
+; Hint: maintain a list of visited pairs
+
+(define (count-pairs-2 x)
+  (define (already-visited? a b)
+    ; is a in b?
+    (if (null? b)
+        #f
+        (if (eq? a (car b))
+            #t
+            (already-visited? a (cdr b)))))
+  (define visited (list 999))
+  (define (count-pairs-visited y)
+      (if (not (pair? y))
+          0
+          (let ((been-here (already-visited? y visited)))
+            (append! visited (list y)) ; changes visited
+            (if been-here
+                0
+                (+ (count-pairs-visited (car y))
+                   (count-pairs-visited (cdr y))
+                   1))
+            )))
+  (count-pairs-visited x))
+
+; These all really have 3 pairs in them
+(= 3 (count-pairs-2 pair-k))
+(= 3 (count-pairs-2 pair-1))
+(= 3 (count-pairs-2 pair-b))
+
+"Exercise 3.18 : Contains-cycle"
+(define (cycles? a_list)
+  (define (already-visited? a b)
+    ; is a in b?
+    (if (null? b)
+        #f
+        (if (eq? a (car b))
+            #t
+            (already-visited? a (cdr b)))))
+  (define visited (list))
+  (define (inner-cycles? a_list)
+    (if (null? a_list)
+        #f
+        (if (already-visited? a_list visited)
+            #t
+            (begin (set! visited (cons a_list visited))
+                   (inner-cycles? (cdr a_list))))))
+  (inner-cycles? a_list))
+
+(cycles? (list 1))
+(define list_cycles (list 1 2 3))
+(make-cycle list_cycles)
+(cycles? list_cycles)
+
+"Exercise 3.19 : Constant-space contains-cycle"
+; Above, we make use of the "visited" list, which adds linear memory.
+(define (make-cheeky-cycle a_list)
+  ; makes a list with a cycle on the last element
+  ; from  1,-> 2,-> 3,()
+  ; to    1,-> 2,-> 3,|
+  ;                 ^--
+  (if (null? (cdr a_list))
+      (set-cdr! a_list a_list)
+      (make-cheeky-cycle (cdr a_list))))
+
+(define list_cheeky_cycles (list 1 2 3))
+(make-cheeky-cycle list_cheeky_cycles)
+list_cheeky_cycles
+(cycles? list_cheeky_cycles)
+
+(define (cs-cycles? a_list)
+  ; constant-space cycles
+  ;  a_list: a list that may, or may not, cycle
+  ;  returns: #t or #f
+  (define (in-list? the-list stop-at item)
+    ; go step through the-list
+    ; if we encounter item, return #t
+    ; if we get to stop-at, return #f
+    (cond ((eq? the-list item) #t)
+          ((eq? the-list stop-at) #f)
+          (else (in-list? (cdr the-list) stop-at item))))
+  (define (inner-cycles? c_list)
+    (cond ((null? (cdr c_list)) #f)
+          ((in-list? a_list c_list (cdr c_list)) #t)
+          (else (inner-cycles? (cdr c_list)))))
+  (inner-cycles? a_list))
+
+(cs-cycles? list_cheeky_cycles)
+(cs-cycles? list_cycles)
+(cs-cycles? (list 1 2 3 4 5))
