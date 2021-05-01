2cb06dd9f462d37547b204fa634cec21de753e68
Iain S <i.stenson@qmul.ac.uk>
2021-05-01 19:00:37 +0100

    Complete queue exercises

--- 3.0.rkt
+++ 3.0.rkt
@@ -552,3 +552,104 @@ list_cheeky_cycles
 (cs-cycles? list_cheeky_cycles)
 (cs-cycles? list_cycles)
 (cs-cycles? (list 1 2 3 4 5))
+
+;; Queues
+
+(define (front-ptr queue) (car queue))
+
+(define (rear-ptr queue) (cdr queue))
+
+(define (set-front-ptr! queue item) 
+  (set-car! queue item))
+
+(define (set-rear-ptr! queue item) 
+  (set-cdr! queue item))
+
+(define (empty-queue? queue) 
+  (null? (front-ptr queue)))
+
+(define (make-queue) (cons '() '()))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an 
+              empty queue" queue)
+      (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else (set-cdr! (rear-ptr queue) 
+                          new-pair)
+                (set-rear-ptr! queue new-pair)
+                queue))))
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with 
+                 an empty queue" queue))
+        (else (set-front-ptr! 
+               queue 
+               (cdr (front-ptr queue)))
+              queue)))
+
+"Exercise 3.21"
+(define (print-queue queue)
+  (display (front-ptr queue))
+  (display "\n"))
+
+
+(define q1 (make-queue))
+(insert-queue! q1 'a)
+(insert-queue! q1 'b)
+
+(print-queue q1)
+(delete-queue! q1)
+(delete-queue! q1)
+(print-queue q1)
+
+
+"Exercise 3.22"
+(define (make-queue2)
+  (let ((front-ptr '())
+        (rear-ptr '()))
+    (define (insert item)
+      (let ((new-pair (cons item '())))
+        (cond ((null? front-ptr)
+               (set! front-ptr new-pair)
+               (set! rear-ptr new-pair))
+              (else (set-cdr! rear-ptr new-pair)
+                    (set! rear-ptr new-pair)))
+        )
+      front-ptr)
+    (define (delete)
+      ; remove the first item
+      (set! front-ptr (cdr front-ptr))
+      front-ptr)
+    (define (dispatch m)
+      (cond ((eq? m 'insert) insert)
+            ((eq? m 'front) (car front-ptr))
+            ((eq? m 'isempty) (null? front-ptr))
+            ((eq? m 'delete) delete)
+            (else (error "Undefined operation: CONS" m)))) 
+    dispatch))
+
+(define q2 (make-queue2))
+;(define q3 (make-queue2))
+
+;; we won't bother with convenience functions for these
+((q2 'insert) 77)
+((q2 'insert) 79)
+(q2 'front)
+(q2 'isempty)
+((q2 'delete))
+((q2 'delete))
+(q2 'isempty)
+"done"
+
+"Exercise 3.23"
+"No, I am not writing a deque"
+
